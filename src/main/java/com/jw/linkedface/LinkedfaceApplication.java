36654557bd027f8dc8be45b4c13953cacd6aaba7
JianquanWang <jwang438@uottawa.ca>
2021-04-03 00:48:20 -0400

    construct spring boot project

---
+++ src/main/java/com/jw/linkedface/LinkedfaceApplication.java
@@ -0,0 +1,13 @@
+package com.jw.linkedface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkedfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jw/linkedface/LinkedfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.linkedface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
